05262ee81e58b043f77aadc13d1852af4e10ed6a
tnt2017 <tnt2011@bk.ru>
2017-05-25 13:42:24 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace hlr_checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox_column = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_id_column = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_region_column = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load csv ...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Order phones";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(185, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(975, 459);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Send HLR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Check HLR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 157);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Mass Check HLR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 128);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(158, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Mass Send HLR";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 238);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(158, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Save csv";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(185, 478);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(975, 23);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // textBox_column
+            // 
+            this.textBox_column.Location = new System.Drawing.Point(95, 287);
+            this.textBox_column.Name = "textBox_column";
+            this.textBox_column.Size = new System.Drawing.Size(66, 20);
+            this.textBox_column.TabIndex = 13;
+            this.textBox_column.Text = "A0";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox_id_column
+            // 
+            this.textBox_id_column.Location = new System.Drawing.Point(95, 319);
+            this.textBox_id_column.Name = "textBox_id_column";
+            this.textBox_id_column.Size = new System.Drawing.Size(66, 20);
+            this.textBox_id_column.TabIndex = 14;
+            this.textBox_id_column.Text = "A2";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 400);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Phone cell";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "ID cell";
+            // 
+            // textBox_region_column
+            // 
+            this.textBox_region_column.Location = new System.Drawing.Point(95, 354);
+            this.textBox_region_column.Name = "textBox_region_column";
+            this.textBox_region_column.Size = new System.Drawing.Size(66, 20);
+            this.textBox_region_column.TabIndex = 18;
+            this.textBox_region_column.Text = "E1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Region cell";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(112, 195);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(58, 20);
+            this.numericUpDown1.TabIndex = 20;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1200,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Start from";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 507);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_region_column);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox_id_column);
+            this.Controls.Add(this.textBox_column);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "HLR CHECKER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox_column;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox_id_column;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_region_column;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Globalization;
+
+namespace hlr_checker
+{
+    public partial class Form1 : Form
+    {
+        string colname = "A4";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //@"D:\пробники_22052017.csv";
+
+
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "D:\\пробники_22052017\\";
+            openFileDialog1.Filter = "csv files (*.csv)|*.csv|txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            dataGridView1.DataSource = ReadCSVFile(openFileDialog1.FileName); // @"C:\Users\User\Desktop\jili-bili.csv"
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+            try
+            {
+                dataGridView1.Columns["E0"].Width = 150;
+                dataGridView1.Columns["E1"].Width = 150;
+                dataGridView1.Columns["E2"].Width = 400;
+            }
+            catch
+            {
+
+            }
+
+            colname = textBox_column.Text;
+        }
+
+        private DataTable ReadCSVFile(string pathToCsvFile)
+        {
+            //создаём таблицу
+            DataTable dt = new DataTable("Cars");
+
+            try
+            {
+                DataRow dr = null;
+                string[] carValues = null;
+                string[] cars = File.ReadAllLines(pathToCsvFile, Encoding.GetEncoding(1251));
+                carValues = cars[0].Split(';'); // названия столбцов
+                
+                //MessageBox.Show(carValues.Length.ToString());
+
+                for (int i = 0; i < carValues.Length; i++)
+                {
+                    dt.Columns.Add("A"+i, typeof(String));
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    dt.Columns.Add("E" + i, typeof(String));
+                }
+
+                for (int i = 0; i < cars.Length; i++)
+                {
+                    if (!String.IsNullOrEmpty(cars[i]))
+                    {
+                        carValues = cars[i].Split(';');
+                        //carValues.Length;
+                        //создаём новую строку
+                        dr = dt.NewRow();
+
+                        for (int j = 0; j < carValues.Length; j++)
+                        {
+                            dr["A" + j] = carValues[j];
+                        }
+
+                        //добавляем строку в таблицу
+                        dt.Rows.Add(dr);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return dt;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                string s = dataGridView1.Rows[i].Cells[colname].Value.ToString();
+                s = s.Replace("-", "");
+                s = s.Replace("\"", "");
+                s = s.Replace(" ", "");
+
+                if (s.Length == 10)
+                    s = "7" + s;
+
+                dataGridView1.Rows[i].Cells[colname].Value = s;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                colname = textBox_column.Text;
+                string phone = dataGridView1.CurrentRow.Cells[colname].Value.ToString();
+                //MessageBox.Show(phone);
+
+                string site = "https://smsc.ru/sys/send.php?login=idbonuskz&psw=FZ9Zabk8Z&phones=" + phone + "&hlr=1";
+
+                HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(site);
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+                using (StreamReader stream = new StreamReader(
+                     resp.GetResponseStream(), Encoding.UTF8))
+                {
+                    string Text = stream.ReadToEnd();
+                    dataGridView1.CurrentRow.Cells["E0"].Value = Text;
+                    //MessageBox.Show(Text);
+                }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            colname = textBox_column.Text;
+            string phone = dataGridView1.CurrentRow.Cells[colname].Value.ToString();
+            string ID = dataGridView1.CurrentRow.Cells[textBox_id_column.Text].Value.ToString();
+
+            if (ID == "")
+            {
+                    MessageBox.Show("Не отправили HLR запрос");
+                    return;
+            }
+
+            try
+            {
+                ID = ID.Substring(ID.Length - 6, 6);
+            }
+            catch
+            {
+
+            }
+                SMSC smsc = new SMSC();
+
+                string[] r = smsc.get_status(ID, phone, 2);
+
+                //MessageBox.Show(r.Count().ToString());
+                string Text = "";
+                foreach (string s in r)
+                {
+                    Text += s + "\r\n";
+                }
+
+                //MessageBox.Show(Text);
+
+                if (r.Count() > 15)
+                {
+                    dataGridView1.CurrentRow.Cells["E0"].Value = r[15];
+                    dataGridView1.CurrentRow.Cells["E1"].Value = r[18];
+                    dataGridView1.CurrentRow.Cells["E2"].Value = r[19];
+                //textBox_region_column
+            }
+
+                Application.DoEvents();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = dataGridView1.RowCount;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.CurrentCell = dataGridView1[0, i];
+                button3_Click(null, null);
+                this.Text = "HLR CHECKER " +  i.ToString() + " из " + dataGridView1.RowCount.ToString();
+                progressBar1.Value = i;
+                Application.DoEvents();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = dataGridView1.RowCount;
+            for (int i = Convert.ToInt32(numericUpDown1.Value); i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.CurrentCell = dataGridView1[0, i];
+                this.Text = "HLR CHECKER " + i.ToString() + " из " + dataGridView1.RowCount.ToString();
+                button4_Click(null, null);
+                progressBar1.Value = i;
+                Application.DoEvents();
+            }
+        }
+
+        private void SaveToCSV(DataGridView DGV)
+        {
+            string filename = "";
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "CSV (*.csv)|*.csv";
+            sfd.FileName = "Output.csv";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Data will be exported and you will be notified when it is ready.");
+                if (File.Exists(filename))
+                {
+                    try
+                    {
+                        File.Delete(filename);
+                    }
+                    catch (IOException ex)
+                    {
+                        MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
+                    }
+                }
+                int columnCount = DGV.ColumnCount;
+                string columnNames = "";
+                string[] output = new string[DGV.RowCount + 1];
+                for (int i = 0; i < columnCount; i++)
+                {
+                    columnNames += DGV.Columns[i].Name.ToString() + ";";
+                }
+                output[0] += columnNames;
+                for (int i = 1; (i - 1) < DGV.RowCount-1; i++)
+                {
+                    for (int j = 0; j < columnCount; j++)
+                    {
+                        output[i] += DGV.Rows[i - 1].Cells[j].Value.ToString() + ";";
+                    }
+                }
+                System.IO.File.WriteAllLines(sfd.FileName, output, System.Text.Encoding.UTF8);
+                MessageBox.Show("Your file was generated and its ready for use.");
+            }
+        }
+        
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SaveToCSV(dataGridView1);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        public static string UCS2ToString(string inText)
+        {
+            string res = "";
+            if ((inText.Length == 0) || ((inText.Length % 2) != 0))
+            {
+                return null;
+            }
+            int num = inText.Length / 2;
+            byte[] buffer = new byte[num];
+            for (int i = 0; i < num; i++)
+            {
+                buffer[i] = byte.Parse(inText.Substring(i * 2, 2), NumberStyles.HexNumber);
+            }
+            res = Encoding.BigEndianUnicode.GetString(buffer);
+            return res;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(UCS2ToString("04110430043B0430043D0441003A002000320032002C0035000A041F043E043F043E043B043D04380442044C00200441044704350442003A000A0031003E041F043E043C043E0449044C002004340440044304330430000A0032003E04210020043A043004400442044B000A0033003E0050004C004100590042004F0059002E00310038002B002E00370434002F00300440"));
+            MessageBox.Show(UCS2ToString("AA180C3602"));
+
+        }
+    }
+}
